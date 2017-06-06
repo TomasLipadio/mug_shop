fb5b31bb980a204e66a46f7f306d9350747a840e
TomasLipadio <zhenya-pershikov@mail.ru>
2017-06-06 22:20:47 +0700

    Update register.php

--- register.php
+++ register.php
@@ -8,30 +8,30 @@
                <h2 class="post_ttl2">УСПЕШНАЯ РЕГИСТРАЦИЯ</h2>
                <div id="basket" style="background:none;">
                <?php 
-					if (isset($_SESSION['login'])) {
-							echo '<script>location.href="index.php"</script>';
-						}
-					else {
-						$login=htmlspecialchars($_POST['login']);
-						$password=htmlspecialchars($_POST['password']);
-						$logins=file('files/loginsF2j7Jed82hQ3BkLw2oQnsK.txt', FILE_IGNORE_NEW_LINES);
+			if (isset($_SESSION['login'])) {
+				echo '<script>location.href="index.php"</script>';
+			}
+			else {
+				$login=htmlspecialchars($_POST['login']);
+				$password=htmlspecialchars($_POST['password']);
+				$logins=file('files/loginsF2j7Jed82hQ3BkLw2oQnsK.txt', FILE_IGNORE_NEW_LINES);
 						
-						for ($i=0; $i<count($logins); $i++)
-						{
-							if ($login==$logins[$i])
-							{
-								$_SESSION['login_already_exist']=true;
-								echo '<script>location.href="registration.php"</script>';
-								exit;
-							}
-						}
-						
-						file_put_contents('files/loginsF2j7Jed82hQ3BkLw2oQnsK.txt', $login."\n", FILE_APPEND);
-						file_put_contents('files/passwordsIo7x9R2u7RWn1Jq7G2S1k2.txt', $password."\n", FILE_APPEND);
-						$_SESSION['login']=$login;
-						echo '<p align="center" style="font-size:24px;color:#540099;font-weight:bold;"> Вы успешно зарегистрированы!</p>';
+				for ($i=0; $i<count($logins); $i++)
+				{
+					if ($login==$logins[$i])
+					{
+						$_SESSION['login_already_exist']=true;
+						echo '<script>location.href="registration.php"</script>';
+						exit;
 					}
-				?>
+				}
+						
+				file_put_contents('files/loginsF2j7Jed82hQ3BkLw2oQnsK.txt', $login."\n", FILE_APPEND);
+				file_put_contents('files/passwordsIo7x9R2u7RWn1Jq7G2S1k2.txt', $password."\n", FILE_APPEND);
+				$_SESSION['login']=$login;
+				echo '<p align="center" style="font-size:24px;color:#540099;font-weight:bold;"> Вы успешно зарегистрированы!</p>';
+			}
+		?>
                </div>
             </div>
          </div>
